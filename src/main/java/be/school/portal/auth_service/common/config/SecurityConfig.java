91ae05dbc1f44c2c49f58ebc240cecaa685d1847
lels-ubuntu <francis.jore..canuto@gmail.com>
2025-10-22 18:32:01 +0800

    add cors config

--- src/main/java/be/school/portal/auth_service/common/config/SecurityConfig.java
+++ src/main/java/be/school/portal/auth_service/common/config/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,10 +39,14 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests(
             auth ->
+
                 // Public auth endpoints
-                auth.requestMatchers("/api/auth/login", "/api/auth/token/refresh")
+                auth.requestMatchers(HttpMethod.OPTIONS, "/**")
+                    .permitAll()
+                    .requestMatchers("/api/auth/login", "/api/auth/token/refresh")
                     .permitAll()
                     // Swagger endpoints
                     .requestMatchers(
